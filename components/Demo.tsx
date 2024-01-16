a9d075a73056001cd03d24058c93480684344205
Iqtidartara <135112726+Iqtidartara@users.noreply.github.com>
2024-01-17 02:13:04 +0500

    refractor :Demo section

--- components/Demo.tsx
+++ components/Demo.tsx
@@ -5,11 +5,11 @@ import { url } from "inspector";
 
 const Demo = () => {
   return (
-    <section className="w-full">
+    <section className="w-full py-16">
       <div
       style={
         {
-          background :"url('/bgdemo.svg')",
+          background :"url('/demo.png')",
           backgroundPosition : "center",
           backgroundRepeat:'no-repeat',
           backgroundSize:'cover',
@@ -23,13 +23,14 @@ const Demo = () => {
         <div className="flex flex-row justify-between">
           {/* first image */}
           <div className="w-1/3">
-            <Image src="/demo1.png" alt="first image" width={525} height={324} className="rounded-[1.625rem]" />
+            
           </div>
 
           {/* buttons and image */}
-          <div className="flex flex-col gap-4 m-10 items-center justify-between">
+          <div className="flex flex-col gap-4 items-center justify-between w-1/3 mt-10">
            
-           <div className="flex flex-col gap-4 m-10 w-full"> <Button className=" text-[1.125rem] font-500 uppercase px-6 py-8 bg-[#232323] rounded-[3.23125rem]   border-[1.65px] border-solid border-[#403A35] gap-4 ">
+           <div className="flex flex-col gap-4  w-full"> 
+           <Button className=" text-[1.125rem] font-500 uppercase px-6 py-8 bg-[#232323] rounded-[3.23125rem]   border-[1.65px] border-solid border-[#403A35] gap-4 ">
               Shop Now
               <Image
                 src="/Arrow 12.png"
@@ -41,18 +42,12 @@ const Demo = () => {
             </Button>
             <Button className="bg-[#FBFAF6] border-[1.65px] border-solid border-[#403A35] px-6 py-8 text-[#232323] text-[1.125rem] font-500 uppercase text-center rounded-[3.23125rem] hover:text-[#fff]">explore more COLLECTION</Button>
             </div>
-            <Image
-              src={"/demo2.png"}
-              alt="second image"
-              width={530}
-              height={325}
-              className=""
-            />
+           
           </div>
 
           {/* last image */}
           <div className="w-1/3  ">
-            <Image src="/demo3.png" alt="third image" width={530} height={530} className="w-[33.125rem]" />
+           
           </div>
         </div>
       </div>
